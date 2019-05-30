1c33c217ba0f42c416c2700af26be39955664b48
idealitsol <idealitsol@gmail.com>
2019-05-30 14:31:51 +0100

    added IsValidUUID

--- uuid.go
+++ uuid.go
@@ -9,6 +9,7 @@ import (
 	"crypto/rand"
 	"io"
 	"net"
+	"regexp"
 	"sync/atomic"
 	"time"
 )
@@ -112,4 +113,10 @@ func (u UUID) String() string {
 	r[23] = '-'
 	return string(r)
 
+}
+
+// IsValidUUID checks whether passed in uuid is valid
+func IsValidUUID(uuid string) bool {
+	r := regexp.MustCompile("^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$")
+	return r.MatchString(uuid)
 }
