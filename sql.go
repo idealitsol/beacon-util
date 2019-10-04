a835ae544241701db582360a62b3d0f36c7b33fa
E.K.B <bekintest@gmail.com>
2019-10-05 00:08:16 +0100

    add converter for interface to postgres jsonb

---
+++ sql.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// ConvertInterfaceToPostgresJSONB converts a field in an jsonlike interface form to postgres jsonb type
+func ConvertInterfaceToPostgresJSONB(modelMap interface{}, key string) postgres.Jsonb {
+	switch v := modelMap.(type) {
+	default:
+		fmt.Printf("unexpected type %T", v)
+	case map[string]interface{}:
+		modelMap = modelMap.(map[string]interface{})[key]
+	case []interface{}:
+		modelMap = modelMap.(map[string]interface{})[key]
+	}
+
+	jsonEnc, err := json.Marshal(modelMap)
+	if err != nil {
+		panic("Could not marshal json")
+	}
+
+	return postgres.Jsonb{json.RawMessage(jsonEnc)}
+}
